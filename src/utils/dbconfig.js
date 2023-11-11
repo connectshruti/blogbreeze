b5a6357a631777b6f192da69dd09881bbf292b33
connectshruti <138186737+connectshruti@users.noreply.github.com>
2023-11-11 11:51:50 +0530

    Update dbconfig.js

--- src/utils/dbconfig.js
+++ src/utils/dbconfig.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
   } catch (error) {
     throw new Error("Connection failed");
   }
