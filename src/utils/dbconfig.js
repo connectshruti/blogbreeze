39ef6461159a6323c277e99dbe239773920348ce
Shruti <shrutisharmaconnect@gmail.com>
2023-11-13 09:56:26 +0530

    Added Comment

--- src/utils/dbconfig.js
+++ src/utils/dbconfig.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const connectDatabase = async () => {
   try {
+    //kya krun ram mujhe budha mil gya
     await mongoose.connect(process.env.MONGO_URL);
   } catch (error) {
     throw new Error("Connection failed");
